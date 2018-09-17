9741b1e49cb48fea770352d44d3d4d90d7aa0a62
neerajpadarthi <42651065+neerajpadarthi@users.noreply.github.com>
2018-09-17 12:43:44 -0500

    Lab Assign

--- js/main.js
+++ js/main.js
@@ -1,3 +1,133 @@
+var defaultLayers, map;
+var locationsContainer;
+
+
+
+
+
+
+function showFunction()
+{
+	 var x = document.getElementById("menu-4");
+                   
+                    if (x.style.display === "none") {
+                        x.style.display = "block";
+                    }
+}
+
+function showFunction1()
+{
+    var x = document.getElementById("menu-2");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+}
+
+ function loadMap(){
+ window.alert("Loaded");
+
+     // Step 1: initialize communication with the platform
+     var platform = new H.service.Platform({
+         app_id: 'devportal-demo-20180625',
+         app_code: '9v2BkviRwi9Ot26kp2IysQ',
+         useHTTPS: true
+     });
+
+
+     var pixelRatio = window.devicePixelRatio || 1;
+      defaultLayers = platform.createDefaultLayers({
+         tileSize: pixelRatio === 1 ? 256 : 512,
+         ppi: pixelRatio === 1 ? undefined : 320
+     });
+
+// Step 2: initialize a map
+     map = new H.Map(document.getElementById('map'), defaultLayers.normal.map, {
+         // initial center and zoom level of the map
+         zoom: 5,
+         center: {lat: 20.5937, lng: 78.9629},
+         pixelRatio: pixelRatio
+     });
+
+
+      locationsContainer = document.getElementById('panel');
+
+// Step 3: make the map interactive
+// MapEvents enables the event system
+// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Step 4: main logic goes here
+     addMetaInfoLayer(map);
+
+
+ }
+function addMetaInfoLayer(map) {
+  // Subscribe to the metaInfo objects' pointermove event. The event bubbles up to
+  // the TileProvider of the metaInfo layer.
+  var tileProvider = defaultLayers.normal.metaInfo.getProvider();
+  tileProvider.addEventListener('pointermove', updateMapCursor);
+
+  // Change cursor back when not needed
+  map.addEventListener('pointermove', updateMapCursor);
+
+  // Add a metaInfo layer to the map
+  map.addLayer(defaultLayers.normal.metaInfo);
+}
+
+
+/**
+ * Sets cursor to pointer, when hovering over meta objects
+ * @this {H.Map}
+ * @param {H.mapevents.Event} e
+ */
+function updateMapCursor(e) {
+  // Change cursor appearance when hovering over geographic objects
+  map.getElement().style.cursor = (e.target === map) ? '' : 'pointer';
+  if (e.target !== map){
+    showMetaInfo(e);
+  }
+}
+
+
+/**
+ * Handler for metaInfo tap event
+ *
+ * @param {H.mapevents.Event} e Fired event
+ */
+function showMetaInfo(e) {
+  var currentPointer = e.currentPointer,
+      metaInfoData = e.target.getData(),
+      // Format object's data for display
+      content =  '<strong style="font-size: large;">' + metaInfoData.name  + '</strong></br>';
+      content  += '<br/><strong>metaInfo:</strong><br/>';
+      content  += '<div style="margin-left:5%; margin-right:5%;"><pre><code style="font-size: x-small;">' +
+        JSON.stringify(metaInfoData, null, ' ') + '</code></pre></div>';
+		
+		
+		
+		console.log(typeof(JSON.stringify(metaInfoData, null, ' ')));
+		
+		// var myObj = JSON.parse(JSON.stringify(metaInfoData, ' ', '-'));
+		// console.log(typeof(myObj));
+
+  // locationsContainer.innerHTML = metaInfoData.name+"   " +metaInfoData["city center info"]+content;
+
+  console.log("Value is "+metaInfoData.name);
+  putCity=document.getElementById("city");
+  putCity.innerText = metaInfoData.name;
+    putCountry=document.getElementById("country");
+    putCountry.innerText = metaInfoData["city center info"]["country code"];
+    putLatitude=document.getElementById("latitude");
+    putLatitude.innerText = metaInfoData["city center info"].position.latitude;
+    putLongitude=document.getElementById("longitude");
+    putLongitude.innerText = metaInfoData["city center info"].position.longitude;
+    putPopulation=document.getElementById("population");
+    putPopulation.innerText = metaInfoData["city center info"].population;
+}
+
+
+
 (function($) {
 
 	"use strict";	
@@ -32,13 +162,14 @@
 
 	$(".main-menu a.projectbutton").click(function(){
 		$("#menu-container .content").slideUp('slow');
-		$("#menu-container .gallery-section").slideDown('slow');
+		$("#menu-container .pro-section").slideDown('slow');
 		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
 		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
 		return false;
 	});
 
 	$(".main-menu a.contactbutton").click(function(){
+        // $("#menu-container .content").css("display", "block");
 		$("#menu-container .content").slideUp('slow');
 		$("#menu-container .contact-section").slideDown('slow');
 		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
