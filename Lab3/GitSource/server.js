6ae7cdd39829111c5031fa0f8ef8369d1d108203
neerajpadarthi <42651065+neerajpadarthi@users.noreply.github.com>
2018-10-30 10:56:14 -0500

    LAb3

--- Lab3/GitSource/server.js
+++ Lab3/GitSource/server.js
@@ -4,6 +4,64 @@ var app = express();
 const nodemailer = require ('nodemailer');
 const xoauth2 =  require ('xoauth2') ;
 
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+// var url = 'mongodb://root:secure@ds161483.mlab.com:61483/asefall17';
+var url = 'mongodb://appstest:appstest123@ds137003.mlab.com:37003/apps';
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/enroll', function (req, res) {
+    MongoClient.connect(url, function(err, client) {
+        if(err)
+        {
+            res.write("Failed, Error while cosnnecting to Database");
+            res.end();
+        }
+        var db= client.db();
+        insertDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+});
+app.get('/getData', function (req, res) {
+    var searchKeywords = req.query.keywords;
+    console.log("Param are "+searchKeywords);
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while cosnnecting to Database");
+            res.end();
+        }
+        if (err) throw err;
+        var dbo = db.db("apps");
+        var query = { username: searchKeywords };
+        dbo.collection("aselab").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            // console.log(result[0].major);
+            db.close();
+            res.json(result);
+        });
+    });
+});
+var insertDocument = function(db, data, callback) {
+    db.collection('aselab').insertOne( data, function(err, result) {
+        if(err)
+        {
+            res.write("Registration Failed, Error While Registering");
+            res.end();
+        }
+        console.log("Inserted a document into the asedemo collection.");
+        callback();
+    });
+};
+
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
@@ -46,18 +104,14 @@ app.get('/getData', function (req, res) {
         subject : searchKeywords2,
         text : searchKeywords1
     }
-
     transporter.sendMail(mailoption, function (err , res) {
-
         if(err)
         {
             console.log('error' );
         }
         else
-
         {
             console.log('mail sent' );
         }
-
     })
 });
